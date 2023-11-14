b76947e76e1594c0625ea8ded19fcd01cedcc5e0
braydenvanm <64617057+braydenvanm@users.noreply.github.com>
2023-11-13 19:47:48 -0500

    end of the day

--- penguin_cache.py
+++ penguin_cache.py
@@ -6,14 +6,14 @@ import time
 
 st.title("Palmer's Penguins")
 
-st.markdown('Use this Streamlit app to make your own scatterplot about
-penguins!')
+st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 
 penguin_file = st.file_uploader(
     'Select Your Local Penguins CSV (default provided)')
-    
+
+@st.cache_data()    
 def load_file(penguin_file):
-    time.sleep(3)
+    time.sleep(2)
     if penguin_file is not None:
         df = pd.read_csv(penguin_file)
     else:
@@ -27,12 +27,10 @@ selected_x_var = st.selectbox('What do you want the x variable to be?',
 'flipper_length_mm', 'body_mass_g'])
 
 selected_y_var = st.selectbox('What about the y?',
-                              ['bill_depth_mm', 'bill_length_mm',
-'flipper_length_mm', 'body_mass_g'])
+                              ['bill_depth_mm', 'bill_length_mm','flipper_length_mm', 'body_mass_g'])
 
 selected_gender = st.selectbox('What gender do you want to filter for?',
-                               ['all penguins', 'male penguins', 'female
-penguins'])
+                               ['all penguins', 'male penguins', 'female penguins'])
 
 if selected_gender == 'male penguins':
     penguins_df = penguins_df[penguins_df['sex'] == 'male']
